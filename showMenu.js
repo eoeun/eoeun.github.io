ddb44de6c31407061cead7e1a4f4781f5adae649
HelloWorld017 <deu07115@gmail.com>
2015-03-01 22:03:40 +0900

    Updated open menu

--- showMenu.js
+++ showMenu.js
@@ -1,23 +1,36 @@
+/*
+*3-4 Show section
+*/
+"use strict";
+
+var  currentlyShown = "none";
+
 function showDocument(){
-	
+	if(currentlyShown == "document"){
+		document.getElementById("document").style.display="none";
+		currentlyShown = "none";
+	}else{
+		document.getElementById("document").style.display = "inline";
+		currentlyShown = "document";
+	}
 }
 
 function showBoard(){
 
 }
 
-function showCalendar(){
-
-}
-
 function showAbout(){
-}
 
-function showLogIn(){
 }
 
 function showPoll(){
+
 }
 
 function showToDo(){
+
+}
+
+function showLogin(){
+
 }
