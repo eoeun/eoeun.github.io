a74f352907a4c69ded79e3aa41ffd7a96003eb05
HelloWorld017 <deu07115@gmail.com>
2015-03-06 16:26:38 +0900

    Updated login and create_token

--- create_token.php
+++ create_token.php
@@ -14,6 +14,14 @@
 	echo "$token";
 	
 	//TODO add mySQL registration
+	$mySQL = explode(',' , file_get_contents("secure_MySQL.txt"), 2);
+	$mySQLConn = new mysqli('localhost', $mySQL[0], $mySQL[1], 'secure_tokens');
+	
+	if($mySQLConn->connect_error){
+		echo "<script> alert(\"로그인에 에러가 발생했습니다!\") </script>";
+		exit("connect_error");
+	}
+	
 	switch($type){
 		case "id":break;
 	}
---
+++ login.php
@@ -0,0 +1,4 @@
+<?php
+	$pw = $_POST["pw"];
+	echo $pw;
+?>
